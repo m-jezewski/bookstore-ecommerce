7117b70d11c43773c1f9a4d620823650df909451
Karrius <Ecetris@gmail.com>
2023-04-27 03:47:17 +0200

    template cleanup, global styles

---
+++ src/styles/muiTheme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: 'F3F3F3',
+      main: '#457B9D',
+      dark: '080F0F',
+    },
+  },
+  typography: {
+    fontFamily: ['Raleway', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+  },
+});
