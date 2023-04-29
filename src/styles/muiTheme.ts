80e56d94aaf6b39850be2419108aa98ebaec1f1d
Karrius <Ecetris@gmail.com>
2023-04-30 00:43:23 +0200

    theme update

--- src/styles/muiTheme.ts
+++ src/styles/muiTheme.ts
@@ -3,12 +3,21 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   palette: {
     primary: {
-      light: 'F3F3F3',
+      light: '#F3F3F3',
       main: '#457B9D',
-      dark: '080F0F',
+      dark: '#080F0F',
     },
   },
   typography: {
     fontFamily: ['Raleway', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'Roboto',
+        },
+      },
+    },
   },
 });
