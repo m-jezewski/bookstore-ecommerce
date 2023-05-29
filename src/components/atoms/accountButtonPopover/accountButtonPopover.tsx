2a0feb189a947f173ce47e42a03f02a0b19f74ce
Karrius <Ecetris@gmail.com>
2023-05-29 20:07:28 +0200

    accountButton updated with Popover mui component, opens up popover with signup and login buttons

---
+++ src/components/atoms/accountButtonPopover/accountButtonPopover.tsx
@@ -0,0 +1,56 @@
+import { UserIcon } from '@heroicons/react/24/outline';
+import { Button, IconButton, Popover, useMediaQuery } from '@mui/material';
+import { useState } from 'react';
+import AccountPopoverContent from '../accountPopoverContent/accountPopoverContent';
+
+interface AccountButtonPopoverProps {}
+
+// this will (probably, still thinking about that) open popover with login and register links
+
+export const AccountButtonPopover = () => {
+  const upMd = useMediaQuery('(min-width: 900px)');
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClosePopover = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  return (
+    <>
+      {upMd ? (
+        <Button onClick={handleClick} startIcon={<UserIcon height={24} width={24} />}>
+          Account
+        </Button>
+      ) : (
+        <IconButton onClick={handleClick} aria-label='Account'>
+          <UserIcon height={24} width={24} />
+        </IconButton>
+      )}
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClosePopover}
+        sx={{
+          zIndex: 10000,
+          '& .MuiPaper-root': {
+            borderRadius: '0.5rem',
+          },
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}>
+        <AccountPopoverContent />
+      </Popover>
+    </>
+  );
+};
