e613c40cd79c0ac5fa9ad7e61e235f41aafb7fb5
Karrius <Ecetris@gmail.com>
2023-06-03 22:32:19 +0200

    TRQ setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,18 @@ import type { AppProps } from 'next/app';
 import { theme } from '@/styles/muiTheme';
 import { ThemeProvider } from '@mui/material';
 import { Layout } from '@/components/organisms/layout/layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
